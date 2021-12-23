ae3765977df346e82510ea94d7dbe41570af191c
mesu-ai <mesu.s.ss@gmail.com>
2021-12-23 09:55:31 +0600

    add product db

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 const express= require('express');
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
-const cors=require('cors');
 const app=express();
+const cors=require('cors');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId
 
-
-const port= process.env.PORT || 5000;
+const port=process.env.PORT || 5000;
 
 // middleware
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
 
-
-
+const { MongoClient } = require('mongodb');
+const { query } = require('express');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.chgli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 async function run() {
     try {
       
       await client.connect();
       const database = client.db("e-shop");
-      const productDB = database.collection("product");
+      const productCollection = database.collection("products");
 
-    
+      console.log('connect to db');
+
+      //get all products
+      app.get('/products',async(req,res)=>{
+        const cursor=productCollection.find({});
+        const result=await cursor.toArray();
+        res.send(result);
+
+      });
+
+      //get selected product
+      app.get('/products/:id',async(req,res)=>{
+        const id=req.params.id;
+        const quary={_id:ObjectId(id)};
+        const result=await productCollection.findOne(quary);
+        res.send(result);
+
+      });
+
+      //get search product
+
+
       console.log("Connected successfully to server");
 
 
@@ -36,11 +57,7 @@ async function run() {
   run().catch(console.dir);
 
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
 
 app.get('/',(req,res)=>{
     res.send('Hello e-shop');

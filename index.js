8c20f65614009f1449f4cf005670f6ba0eac5a78
mesu-ai <mesu.s.ss@gmail.com>
2022-04-19 10:02:19 +0600

    updated

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const ObjectId = require('mongodb').ObjectId;
 const port=process.env.PORT || 5000;
 
 // middleware
- app.use(cors());
-// app.use(cors({
-//     origin:'https://limitless-fjord-65876.herokuapp.com'
-//   })
-// )
+app.use(cors());
 app.use(express.json());
 
 

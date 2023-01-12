03738393c7fe12e27b20572afbad550c3501e998
Md ibrahim adham mesu <mesu.s.ss@gmail.com>
2023-01-12 13:50:04 +0600

    update cors

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const ObjectId = require('mongodb').ObjectId;
 const port=process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://e-shop-bangla.web.app/',
+  methods: ['GET', 'POST', 'PUT','DELETE']
+}));
 app.use(express.json());
 
 
